9caa8c11986793fd72c5129b973878367e8e3cab
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-09-06 09:40:38 -0600

    check for all members of array being over age 19 with .every method

--- script.js
+++ script.js
@@ -27,6 +27,15 @@ const isAdult = people.some(function (person) {
 console.log({ isAdult });
 // Array.prototype.every() // is everyone 19 or older?
 
+const allAdults = people.every(function (person) {
+  const currentYear = new Date().getFullYear();
+  if (currentYear - person.year >= 19) {
+    return true;
+  }
+});
+
+console.log({ allAdults });
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
